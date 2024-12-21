266cd9fcb0beb75f6feeadb2f75d9cd80b4bf56b
felipemaia <felipe.maia.rj@gmai.com>
2024-12-21 06:59:14 -0300

    ajuste nomes

--- MaiaIO.DinExpressions.CLI/Program.cs
+++ MaiaIO.DinExpressions.CLI/Program.cs
@@ -5,13 +5,13 @@ Console.WriteLine("Hello, World!");
 
 
 var encomandas = new List<Encomenda>() {
-           new Encomenda { Id = 1, EmpresaOrigem = "MG10", EmpresaDestino = "MG45", DataCriacao = new DateTime(2024,3,1) , IsActive = true },
-           new Encomenda { Id = 2, EmpresaOrigem = "MG21", EmpresaDestino = "MG32", DataCriacao = new DateTime(2024, 4, 4), IsActive = false },
-           new Encomenda { Id = 3, EmpresaOrigem = "MG36", EmpresaDestino = "MG4", DataCriacao = new DateTime(2024, 1, 5), IsActive = false }
+           new Encomenda { Id = 1, EmpresaOrigem = "XPTO10", EmpresaDestino = "WHTO30", DataCriacao = new DateTime(2024,3,1) , IsActive = true },
+           new Encomenda { Id = 2, EmpresaOrigem = "XPTO30", EmpresaDestino = "XPTO40", DataCriacao = new DateTime(2024, 4, 4), IsActive = false },
+           new Encomenda { Id = 3, EmpresaOrigem = "PGTO36", EmpresaDestino = "PGTO30", DataCriacao = new DateTime(2024, 1, 5), IsActive = false }
     };
 
 
-var comando = new ListarEncomedaComando { EmpresaOrigem = "MG21" };
+var comando = new ListarEncomedaComando { EmpresaOrigem = "XP" };
 
 var predicate = ExpressionBuilder.FiltroCreate(comando);
 
